95cb1227bf92c8612bbf9aadf1a3cb02bed1184a
Yunitrish <yunthomas006@gmail.com>
2024-07-12 13:00:57 +0800

    google auth added

--- lib/ui/screens/login_screen.dart
+++ lib/ui/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:adaptor_games/ui/screens/auth_gate.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
@@ -13,7 +14,12 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return SignInScreen(
-      providers: [EmailAuthProvider()],
+      providers: [
+        EmailAuthProvider(),
+        GoogleProvider(
+            clientId:
+                "458050746882-079ri7rpq67l68bjuc7er4td8meitp1l.apps.googleusercontent.com"),
+      ],
       actions: [
         AuthStateChangeAction<SignedIn>((context, state) {
           Navigator.pushReplacement(context,
